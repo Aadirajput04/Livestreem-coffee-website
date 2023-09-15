ecaf623e5fa31b08551b0f95fcbed775448ba86e
aaditya singh <aadityasingh396@gmail.com>
2023-09-15 18:18:02 +0530

    half done

---
+++ about-section.php
@@ -0,0 +1,52 @@
+<section class="parent-about-section">
+    <div class="inner-about-section side-padding">
+
+        <div class="left-about-section">
+
+
+            <img src="./img/Group 1.png" alt="">
+
+
+        </div>
+        <div class="about-content-section">
+            <h5>About us</h5>
+            <h2> <span>Chose your </span> favourite</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam non odio in efficitur. Fusce
+                tincidunt purus et dui molestie pharetra sit amet non est. Sed lobortis finibus nulla, id varius eros
+                porta eu. Quisque egestas porta nunc.</p>
+                <div class="radio-lines">
+                    <div class="yellow-radio">
+                        <input type="radio" id="first" name="options" value="first">
+                        <label for="first">Supremo</label>
+                    </div>
+        
+                    <div class="yellow-radio">
+                        <input type="radio" id="second" name="options" value="second">
+                        <label for="second">Late</label>
+                    </div>
+        
+                    <div class="yellow-radio">
+                        <input type="radio" id="third" name="options" value="third">
+                        <label for="third">Cappuccino</label>
+                    </div>
+        
+                </div>
+        
+        
+        
+                <a href="#">
+                    Read More<svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+                        <path
+                            d="M26.4916 14.1712L18.3249 6.0045C18.1049 5.79198 17.8102 5.67439 17.5043 5.67705C17.1984 5.6797 16.9058 5.8024 16.6895 6.01871C16.4732 6.23502 16.3505 6.52763 16.3478 6.83353C16.3451 7.13943 16.4627 7.43413 16.6752 7.65417L22.8504 13.8293H2.33341C2.024 13.8293 1.72725 13.9522 1.50846 14.171C1.28966 14.3898 1.16675 14.6866 1.16675 14.996C1.16675 15.3054 1.28966 15.6022 1.50846 15.821C1.72725 16.0398 2.024 16.1627 2.33341 16.1627H22.8504L16.6752 22.3378C16.5638 22.4455 16.4749 22.5742 16.4138 22.7165C16.3527 22.8589 16.3205 23.012 16.3191 23.1669C16.3178 23.3218 16.3473 23.4754 16.406 23.6188C16.4646 23.7622 16.5512 23.8924 16.6608 24.002C16.7703 24.1115 16.9006 24.1981 17.044 24.2568C17.1873 24.3155 17.341 24.345 17.4959 24.3436C17.6508 24.3423 17.8039 24.3101 17.9462 24.249C18.0886 24.1878 18.2173 24.0989 18.3249 23.9875L26.4916 15.8208C26.7103 15.6021 26.8332 15.3054 26.8332 14.996C26.8332 14.6866 26.7103 14.3899 26.4916 14.1712Z"
+                            fill="#CE8F28" />
+                    </svg>
+                </a>
+        </div>
+        
+    </div>
+
+
+
+
+
+</section>
---
+++ gallery-section.php
@@ -0,0 +1,28 @@
+<section class="gallery-section side-padding">
+
+    <div class="gallery-heading-text">
+        <h2> <span> Our </span>gallery</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam non odio in efficitur. Fusce
+            tincidunt purus
+        </p>
+    </div>
+
+    <div class="gallery-images">
+
+        <img src="./img/gallery-img.png" alt="">
+        <img src="./img/gallery-img.png" alt="">
+        <img src="./img/gallery-img.png" alt="">
+        <img src="./img/gallery-img.png" alt="">
+        <img src="./img/gallery-img.png" alt="">
+        <img src="./img/gallery-img.png" alt="">
+
+
+    </div>
+    <a href="#">View More<svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+            <path
+                d="M26.9916 13.1753L18.8249 5.00865C18.6049 4.79613 18.3102 4.67854 18.0043 4.6812C17.6984 4.68385 17.4058 4.80655 17.1895 5.02286C16.9732 5.23917 16.8505 5.53179 16.8478 5.83768C16.8451 6.14358 16.9627 6.43828 17.1752 6.65832L23.3504 12.8335H2.83341C2.524 12.8335 2.22725 12.9564 2.00846 13.1752C1.78966 13.394 1.66675 13.6907 1.66675 14.0001C1.66675 14.3096 1.78966 14.6063 2.00846 14.8251C2.22725 15.0439 2.524 15.1668 2.83341 15.1668H23.3504L17.1752 21.342C17.0638 21.4496 16.9749 21.5783 16.9138 21.7207C16.8527 21.863 16.8205 22.0161 16.8191 22.171C16.8178 22.3259 16.8473 22.4795 16.906 22.6229C16.9646 22.7663 17.0512 22.8966 17.1608 23.0061C17.2703 23.1157 17.4006 23.2023 17.544 23.2609C17.6873 23.3196 17.841 23.3491 17.9959 23.3478C18.1508 23.3464 18.3039 23.3142 18.4462 23.2531C18.5886 23.192 18.7173 23.1031 18.8249 22.9917L26.9916 14.825C27.2103 14.6062 27.3332 14.3095 27.3332 14.0001C27.3332 13.6908 27.2103 13.3941 26.9916 13.1753Z"
+                fill="#CE8F28" />
+        </svg></a>
+
+
+</section>
