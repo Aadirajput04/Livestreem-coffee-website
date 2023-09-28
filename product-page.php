0ae0390062f2482b3a1aa766ed346c20e2292db9
aaditya singh <aadityasingh396@gmail.com>
2023-09-28 23:54:40 +0530

    livestream review bugs fixed

--- product-page.php
+++ product-page.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
     <link rel="stylesheet" href="./style.css">
     <title>Product | Livestreem Coffee</title>
 </head>
@@ -25,45 +27,193 @@
 
     </section>
 
-<section class="products-page" >
+    <section class="products-page side-padding">
 
-    <div class="manu-head-text">
-
-
-        <h2> <span> Most popular</span> menu</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam non odio in efficitur. Fusce
-            tincidunt purus</p>
-
-    </div>
-    
+        <div class="manu-head-text">
 
 
+            <h2> <span> Most popular</span> menu</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam non odio in efficitur. Fusce
+                tincidunt purus</p>
+
+        </div>
+
+        <div class="product-overview">
+            <div class="left-image-view">
+                <img src="./img/product-page-img.png" alt="">
+            </div>
+
+            <div class="right-text-content">
+                <h1><span>caramel</span> Macchiato</h1>
+                <div class="price">
+                    <p>$15.99</p>
+                    <div class="og-price">
+                        <p>$7.99</p>
+                    </div>
+                </div>
+                <img class="rating" src="./img/startssssssssss.png" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam non odio in efficitur.
+                    Fusce tincidunt purus et dui molestie pharetra sit amet non est. Sed lobortis finibus nulla, id
+                    varius eros porta eu. Quisque egestas porta nunc. Maecenas congue nunc eu lobortis efficitur. Duis
+                    quis placerat arcu. Suspendisse eu risus tempor, pellentesque dolor eget, feugiat nisl. Cras
+                    facilisis quis elit quis rhoncus
+                </p>
+
+                <div class="product-adding-line">
+                    <div class="quantity">
+                        <button class="decrement">-</button>
+                        <input id="quantityInput" type="number" value="1" min="1" max="1000">
+                        <button class="increment">+</button>
+                    </div>
+                    <a href="#">Buy Now <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"
+                            viewBox="0 0 28 28" fill="none">
+                            <path
+                                d="M26.4913 13.1752L18.3247 5.00853C18.1046 4.79601 17.8099 4.67842 17.504 4.68107C17.1981 4.68373 16.9055 4.80643 16.6892 5.02274C16.4729 5.23905 16.3502 5.53166 16.3476 5.83756C16.3449 6.14346 16.4625 6.43816 16.675 6.65819L22.8502 12.8334H2.33317C2.02375 12.8334 1.72701 12.9563 1.50821 13.1751C1.28942 13.3939 1.1665 13.6906 1.1665 14C1.1665 14.3094 1.28942 14.6062 1.50821 14.825C1.72701 15.0438 2.02375 15.1667 2.33317 15.1667H22.8502L16.675 21.3419C16.5636 21.4495 16.4747 21.5782 16.4136 21.7206C16.3524 21.8629 16.3202 22.016 16.3189 22.1709C16.3175 22.3258 16.3471 22.4794 16.4057 22.6228C16.4644 22.7662 16.551 22.8964 16.6605 23.006C16.7701 23.1155 16.9003 23.2022 17.0437 23.2608C17.1871 23.3195 17.3407 23.349 17.4956 23.3477C17.6505 23.3463 17.8036 23.3141 17.946 23.253C18.0883 23.1918 18.2171 23.103 18.3247 22.9915L26.4913 14.8249C26.7101 14.6061 26.8329 14.3094 26.8329 14C26.8329 13.6907 26.7101 13.394 26.4913 13.1752Z"
+                                fill="white" />
+                        </svg></a>
+                    <button class="add-to-cart">Add to Cart <svg xmlns="http://www.w3.org/2000/svg" width="28"
+                            height="28" viewBox="0 0 28 28" fill="none">
+                            <path
+                                d="M26.4913 13.1752L18.3247 5.00853C18.1046 4.79601 17.8099 4.67842 17.504 4.68107C17.1981 4.68373 16.9055 4.80643 16.6892 5.02274C16.4729 5.23905 16.3502 5.53166 16.3476 5.83756C16.3449 6.14346 16.4625 6.43816 16.675 6.65819L22.8502 12.8334H2.33317C2.02375 12.8334 1.72701 12.9563 1.50821 13.1751C1.28942 13.3939 1.1665 13.6906 1.1665 14C1.1665 14.3094 1.28942 14.6062 1.50821 14.825C1.72701 15.0438 2.02375 15.1667 2.33317 15.1667H22.8502L16.675 21.3419C16.5636 21.4495 16.4747 21.5782 16.4136 21.7206C16.3524 21.8629 16.3202 22.016 16.3189 22.1709C16.3175 22.3258 16.3471 22.4794 16.4057 22.6228C16.4644 22.7662 16.551 22.8964 16.6605 23.006C16.7701 23.1155 16.9003 23.2022 17.0437 23.2608C17.1871 23.3195 17.3407 23.349 17.4956 23.3477C17.6505 23.3463 17.8036 23.3141 17.946 23.253C18.0883 23.1918 18.2171 23.103 18.3247 22.9915L26.4913 14.8249C26.7101 14.6061 26.8329 14.3094 26.8329 14C26.8329 13.6907 26.7101 13.394 26.4913 13.1752Z"
+                                fill="#CE8F28" />
+                        </svg></button>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="manu-head-text">
+
+
+            <h2> <span> Related</span> Products</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam non odio in efficitur. Fusce
+                tincidunt purus</p>
+
+        </div>
+
+        <!-- <div class="releted-product-overview">
 
 
 
 
 
+        </div> -->
 
+        <div class="manu-items-list">
 
-
-    
-
-</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            <a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 125.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a> <a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 127.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 126.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 125.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 128.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 129.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 126.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 125.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 127.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 126.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 125.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a><a href="./product-page.php">
+                <div class="image-hover-effect">
+                    <img src="./img/Rectangle 128.png" alt="">
+                    <div class="hover-content">
+                        <p>Caramel Macchiato</p>
+                        <span class="favorite-icon"><img src="./img/heart.png" alt=""></span>
+                        <span class="favorite-icon"><img src="./img/wh3.png" alt=""></span>
+                    </div>
+                </div>
+            </a>
+        </div>
+        
+    </section>
 
 
     <?php
@@ -71,7 +221,29 @@
     include "footer.php";
 
     ?>
+    <script>
+        // script.js
+        document.addEventListener("DOMContentLoaded", function () {
+            const decrementButton = document.querySelector(".decrement");
+            const incrementButton = document.querySelector(".increment");
+            const quantityInput = document.querySelector("#quantityInput");
 
+            decrementButton.addEventListener("click", function () {
+                const currentValue = parseInt(quantityInput.value);
+                if (currentValue > 1) {
+                    quantityInput.value = currentValue - 1;
+                }
+            });
+
+            incrementButton.addEventListener("click", function () {
+                const currentValue = parseInt(quantityInput.value);
+                if (currentValue < 10) {
+                    quantityInput.value = currentValue + 1;
+                }
+            });
+        });
+
+    </script>
 </body>
 
 </html>
